7e7ab6fa4b3e9bff3056ff71a2d69c29fd984d5c
justinkerala <justinkerala@yahoo.com>
2017-05-25 16:06:05 +0530

    cassandra configured.

---
+++ spring-boot-cassandra-4/src/main/java/example/cassandratest/ApplicationConfig.java
@@ -0,0 +1,20 @@
+package example.cassandratest;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.java.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+@Configuration
+@EnableCassandraRepositories
+class ApplicationConfig extends AbstractCassandraConfiguration {
+
+  @Override
+  public String getContactPoints() {
+    return "localhost";
+  }
+
+  @Override
+  protected String getKeyspaceName() {
+    return "mykeyspace";
+  }
+}
